864b8dcb5a459afbbfa1c1e25d4de6b29ab2dcff
polivodichka <stacy99anastasia@gmail.com>
2026-03-10 23:28:49 +0300

    ci: migrate to yarn and add full ci/cd pipeline with e2e tests

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,6 +1,5 @@
 import "./commands";
 import "@testing-library/cypress/add-commands";
-import "@cypress/code-coverage/support";
 
 // Игнор внутренней ошибку Next.js/Turbopack в dev-режиме:
 // "Failed to execute 'measure' on 'Performance': cannot have a negative time stamp"
